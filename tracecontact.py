6e5ab0a95bc39bc292a53809b486a93b067d3a53
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-02 00:31:40 +0800

    Updated default dictionary

--- tracecontact.py
+++ tracecontact.py
@@ -1,8 +1,8 @@
 menu_ = ["Add items","Search items","Modify items","Delete items","Show all data","Exit program"]
 
-main_dictionary = {1:{"Name":"Jonathan Finuliar","Age":20,"Address":"Caloocan","Phone Number":"0809547458","Vaccination Status": "Vaccinated"},
-                   2:{"Name":"Alyza Catambis","Age":15,"Address":"Zambales","Phone Number":"0804578514","Vaccination Status": "Unvaccinated"},
-                   3:{"Name":"Eve Campo","Age":18,"Address":"Batangas","Phone Number":"0958474521","Vaccination Status": "Boosted"}}
+main_dictionary = {"Jonathan Finuliar":{"Age":22,"Address":"Malabon","Phone Number":"09123456789","Vaccination Status": "Vaccinated"},
+                   "Alyza Catambis":{"Age":15,"Address":"Valenzuela","Phone Number":"099991111234","Vaccination Status": "Unvaccinated"},
+                   "Eve Campo":{"Age":18,"Address":"Navotas","Phone Number":"09988842314","Vaccination Status": "Boosted"}}
 
 def display_menu(me):
     print("----Contact tracing!---")
