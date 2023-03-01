85a450a2bdf137fdf1fd2bb4dff86d5eb491a8bc
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-01 21:20:47 +0800

    Created a default dictionary that contain integer as key and dictionary as value

--- tracecontact.py
+++ tracecontact.py
@@ -1,1 +1,5 @@
 menu_ = ["Add items","Search items","Modify items","Delete items","Show all data","Exit program"]
+
+main_dictionary = {1:{"Name":"Jonathan Finuliar","Age":20,"Address":"Caloocan","Phone Number":"0809547458","Vaccination Status": "Vaccinated"},
+                   2:{"Name":"Alyza Catambis","Age":15,"Address":"Zambales","Phone Number":"0804578514","Vaccination Status": "Unvaccinated"},
+                   3:{"Name":"Eve Campo","Age":18,"Address":"Batangas","Phone Number":"0958474521","Vaccination Status": "Boosted"}}
