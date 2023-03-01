95051aa158da1183a1412779681cbdce772c7f9d
NathanielFinuliar <nathaniel.finuliar14@gmail.com>
2023-03-01 21:35:48 +0800

    Added display_menu function which will serve as menu option

--- tracecontact.py
+++ tracecontact.py
@@ -3,3 +3,10 @@ menu_ = ["Add items","Search items","Modify items","Delete items","Show all data
 main_dictionary = {1:{"Name":"Jonathan Finuliar","Age":20,"Address":"Caloocan","Phone Number":"0809547458","Vaccination Status": "Vaccinated"},
                    2:{"Name":"Alyza Catambis","Age":15,"Address":"Zambales","Phone Number":"0804578514","Vaccination Status": "Unvaccinated"},
                    3:{"Name":"Eve Campo","Age":18,"Address":"Batangas","Phone Number":"0958474521","Vaccination Status": "Boosted"}}
+
+def display_menu(me):
+    print("----Contact tracing!---")
+    print("="*20)
+    for i in range(len(me)):
+        print(f"{i+1} -> {me[i]}")
+    print("="*20)
